82e931869b5b227d2b2d41d745f1fb2413dc1875
VuranokYigit <fyvuranokk@gmail.com>
2024-05-01 10:29:54 +0300

    bugFixed

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,9 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History"
-], function(Controller, History) {
+], (Controller, History) => {
     'use strict';
-    return Controller.extends("sapui5hw.controller.Detail",{
+    return Controller.extend("sapui5hw.controller.Detail",{
         onInit() {
 			const oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
